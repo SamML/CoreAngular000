8612b52bf2b40767c46ca3359e24238cd2699e57
samuel89m <samuel89m@gmail.com>
2017-03-25 22:57:28 +0100

    Agregar archivos de proyecto.

---
+++ CoreAngular000/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAngular000.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
